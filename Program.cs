3c42007c44cb8061e4ffaf0a6efe0c4418c2918f
nekimuntu <caribbeanfrenchschool@gmail.com>
2023-02-25 20:26:35 -0600

    intial commit
    CracklePop exercice for RC application

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+  
+        static void CracklePop()
+        {
+                for (int i = 1; i < 10; i++)
+                {
+                    //Check modulos of 3 and print Crackle
+                    if (i % 3 == 0)
+                    {
+                        System.Console.WriteLine(" Crackle");
+                    }
+                    //Check modulos of 5 and print Pop
+                    if (i % 3 == 0)
+                    {
+                        System.Console.WriteLine(" Pop");
+                    }
+                    //Check modulos of 3 and 5 and print CracklePop
+                    if (i % 3 == 0 && i % 5 == 0)
+                    {
+                        System.Console.WriteLine(" CracklePo");
+                    }
+                    if (i % 3 != 0 || i % 5 != 0)
+                    {
+                        System.Console.WriteLine(i);
+                    }
+                }
+
+            }
+            CracklePop();
+    
